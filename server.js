9b5e015a90ae9eb134a1eaeceb6578d17050b506
Leonardo Landin <leolandin123@gmail.com>
2020-02-21 22:07:00 -0300

    remove router of server.js

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const port = 3000;
 const path = require('path');
 const router = express.Router();
 
-// router.get('/', function(req, res) {
-//     console.log('a')
-//     res.sendFile(path.join(__dirname+'/app/index.html'));
-// });
-
 app.use(express.static(path.join(__dirname,'app')));
 
 app.use('/', router);
