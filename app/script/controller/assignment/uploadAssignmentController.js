ff2240db94bbb8762002b1f31f1a15ab7b4f8f01
Leonardo Landin <leolandin123@gmail.com>
2020-06-03 22:49:01 -0300

    validando os campos

--- app/script/controller/assignment/uploadAssignmentController.js
+++ app/script/controller/assignment/uploadAssignmentController.js
@@ -1,19 +1,19 @@
 (function() {
+
+    let assignmentUpload = {};
+
     this.changeImageUpload = function(event) {
         let file = event.target.files.item(0);
         let preview = document.getElementById('previewImage');
-        let imageButton = document.getElementById('selectImageButton');
 
         const reader = new FileReader();
 
         reader.onload = e => preview.src = e.target.result;
         reader.readAsDataURL(file);
-
-        imageButton.style.display = 'block';
+        document.getElementById('containerUpload').style.borderColor = 'black';
     }
 
     this.controlModal = function(param) {
-        console.log(typeof param)
         if(param) {
             document.getElementById('modalUploadContainer').style.display = 'flex';
         } else {
@@ -23,5 +23,60 @@
 
     this.insideModal = function(event) {
         event.stopPropagation();
+    }
+
+    this.submitUpload = function() {
+        let nameAssignment = document.getElementById('nameAssignment').value;
+        let descriptionAssignment = document.getElementById('descriptionAssignment').value;
+        let typeAssignment = document.getElementById('typeAssignment').value;
+        let isAutor = document.getElementById('isAutor').checked;
+        let imageUpload = document.getElementById('selectImage').files.item(0);
+
+        validateAssignment(nameAssignment, descriptionAssignment, typeAssignment, imageUpload)
+
+        assignmentUpload = {
+            nameAssignment: nameAssignment,
+            descriptionAssignment: descriptionAssignment,
+            typeAssignment: typeAssignment,
+            isAutor: isAutor,
+            imageUpload: imageUpload
+        }
+
+
+
+    }
+
+    this.validateAssignment = function(nameAssignment, descriptionAssignment, typeAssignment, imageUpload) {
+        let nameAssignmentElement = document.getElementById('nameAssignment');
+        let descriptionAssignmentElement = document.getElementById('descriptionAssignment');
+        let typeAssignmentElement = document.getElementById('typeAssignment');
+        let imageUploadElement = document.getElementById('selectImage').files.item(0);
+        let containerImage = document.getElementById('containerUpload');
+        let getError = false;
+
+        if(!nameAssignment || !nameAssignment.length) {
+            invalidInput(nameAssignmentElement)
+            getError = true;
+        }
+
+        if(!descriptionAssignment || !descriptionAssignment.length) {
+            invalidInput(descriptionAssignmentElement)
+            getError = true;
+        }
+
+        if(!typeAssignment || parseInt(typeAssignment) == 0) {
+            invalidInput(typeAssignmentElement)
+            getError = true;
+        }
+
+        if(imageUpload === undefined || !imageUpload) {
+            invalidInput(containerImage)
+            document.getElementById('colorInsideContainer').style.color = 'red';
+            getError = true;
+        }
+
+        if(!getError) {
+            return true
+        }
     }
 })()
--- app/script/controller/validateController.js
+++ app/script/controller/validateController.js
@@ -1,13 +1,18 @@
-this.validateInput = function(key) {
+this.validateInput = function(key, color) {
     let keyElement = document.getElementById(key);
 
     if(keyElement.value && keyElement.style.borderColor === 'red') {
-        keyElement.style.borderColor = 'black';
+        if(color !== undefined) {
+            keyElement.style.borderColor = color;            
+        } else {
+            keyElement.style.borderColor = 'black';
+        }
         keyElement.classList.add('validInput');
     } 
 }
 
 this.invalidInput = function(type) {
+
     type.style.borderColor = 'red';
     type.classList.remove('validInput');
     type.classList.add('invalidInput');
